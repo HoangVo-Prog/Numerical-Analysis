351ed3eceeeaecb3fcee15ac6d4916797ee1a176
Hoang Vo~ <vohoang2204@gmail.com>
2025-10-12 21:15:06 +0700

    Edit euler_cli.py

--- euler_cli.py
+++ euler_cli.py
@@ -1,3 +1,22 @@
+# Run this file by create the following bash file: bash run_euler.sh
+# -------------------- run_euler.sh --------------------
+# theta=1
+
+# F='x*y - x**3'
+# EXACT='x**2 + 2 - np.exp(x**2/2)'  
+
+# python euler_cli.py --py "$F" -a 0 -b 2 -N 50 \
+#   --method euler_forward --theta $theta \
+#   --plot --save-plot euler_forward.png \
+#   --exact "$EXACT"
+
+# F='x*y - x**3'
+# EXACT='x**2 + 2 - 5*np.exp(x**2/2 - 2)'
+# python euler_cli.py --py "$F" -a 0 -b 2 -N 50 \
+#   --method euler_backward --theta $theta \
+#   --plot --save-plot euler_backward.png --exact "$EXACT"
+
+# -------------------- euler_cli.py --------------------
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
